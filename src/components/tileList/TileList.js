47acf24914e8f50ac322442e29f0bc7326bff97b
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2025-05-02 12:44:25 +0200

    1/2

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {Tile} from "../tile/Tile"
 
 export const TileList = ({contacts}) => {
   return (
     <div>
-      
+      {contacts.map((contact, i) => {
+        const {name, ...rest} = contact;
+        return <Tile name={name} description={rest} key={i}/>
+      })}
     </div>
   );
 };
